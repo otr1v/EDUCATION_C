c2d4067280314a0d1d4a16a7ff7d2a23170ae4b6
otr1v <100768041+otr1v@users.noreply.github.com>
2023-02-16 17:20:27 +0300

    Create reference.c

---
+++ reference.c
@@ -0,0 +1,7 @@
+//here i will add information about C language(my education)
+
+//firstly, about memset func : memset func converts integer to unsigned char, so it can be used to fill
+// only with 0 or -1 by integers and any symbol of char 
+//in this function it is compulsory to fill lust argument multiplying by sizeof(elem)
+
+// fprintf(FILE* fp, "text like in printf", numbers if there are any identificators (%))
